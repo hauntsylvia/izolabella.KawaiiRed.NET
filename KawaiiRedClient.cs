9c1bd4eef75ba9e393f224870e28c1c305835fb7
Izolabella R. Geddis <iza.yy10@gmail.com>
2022-07-10 15:55:46 -0400

    new analyzers

--- KawaiiRedClient.cs
+++ KawaiiRedClient.cs
@@ -1,5 +1,6 @@
 ﻿using izolabella.KawaiiRed.NET.Enums;
 using Newtonsoft.Json;
+using System.Globalization;
 
 namespace izolabella.KawaiiRed.NET
 {
@@ -16,7 +17,7 @@ namespace izolabella.KawaiiRed.NET
 
         public async Task<GenericResponse?> GetGifAsync(GenericEndpointType Type)
         {
-            HttpResponseMessage Message = await this.Client.GetAsync(BaseAddres + $"gif/{Type.Singular.ToLower()}/token%3Danonymous/");
+            HttpResponseMessage Message = await this.Client.GetAsync(BaseAddres + $"gif/{Type.Singular.ToLower(CultureInfo.InvariantCulture)}/token%3Danonymous/");
             string A = await Message.Content.ReadAsStringAsync();
             return JsonConvert.DeserializeObject<GenericResponse>(A);
         }
