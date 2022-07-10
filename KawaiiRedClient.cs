0a5cb1d9ff012dcd07ff8e2c8d562a17101449cf
Izolabella R. Geddis <iza.yy10@gmail.com>
2022-07-10 16:18:34 -0400

    removed "this." accessor to remain consistent

--- KawaiiRedClient.cs
+++ KawaiiRedClient.cs
@@ -8,7 +8,7 @@ namespace izolabella.KawaiiRed.NET
     {
         public KawaiiRedClient()
         {
-            this.Client = new();
+            Client = new();
         }
 
         public HttpClient Client { get; }
@@ -17,7 +17,7 @@ namespace izolabella.KawaiiRed.NET
 
         public async Task<GenericResponse?> GetGifAsync(GenericEndpointType Type)
         {
-            HttpResponseMessage Message = await this.Client.GetAsync(BaseAddres + $"gif/{Type.Singular.ToLower(CultureInfo.InvariantCulture)}/token%3Danonymous/");
+            HttpResponseMessage Message = await Client.GetAsync(BaseAddres + $"gif/{Type.Singular.ToLower(CultureInfo.InvariantCulture)}/token%3Danonymous/");
             string A = await Message.Content.ReadAsStringAsync();
             return JsonConvert.DeserializeObject<GenericResponse>(A);
         }
